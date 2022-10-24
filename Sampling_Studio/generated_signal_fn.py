786c5dd366be2b89c500434793a8bc18d358d182
Ahmed Emad Eldeen Ahmed <ahmedemad9112@gmail.com>
2022-10-24 22:25:58 +0200

    Updates 1.1

--- Sampling_Studio/generated_signal_fn.py
+++ Sampling_Studio/generated_signal_fn.py
@@ -1,7 +1,8 @@
 # ------------------------------------------------------------------------------------Importing liberaries
+from turtle import color
 from matplotlib.lines import lineStyles
 import streamlit as st
-from numpy import sin,pi,linspace,zeros,arange,mean,sqrt,random,resize,sum,sinc,ceil
+from numpy import sin,pi,linspace,zeros,arange,mean,sqrt,random,resize,sum,sinc,ceil,meshgrid,hypot
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -36,7 +37,7 @@ def general_signal_plotting(x_axis,y_axis):
 # ------------------------------------------------------------------------------------Setting Global Variables
 list_of_objects = []
 initial_time = linspace(0,2, 1000)
-total_signals = zeros(1000)
+total_signals = sin(2*pi*initial_time)
 
 # ------------------------------------------------------------------------------------Signal Object
 class Signal:
@@ -72,14 +73,14 @@ def add_signal():
         remove_button = st.button('Clear', key="Remove Button 22")
     if remove_button and len(list_of_objects)>0: 
         removing_signal(removed_signal_freq,removed_signal_amp) 
+        st.experimental_rerun()
 
     # general_signal_plotting(initial_time,total_signals)
-
     return total_signals
 # ------------------------------------------------------------------------------------Adding Signals
 def adding_signals(frequency,amplitude):                                              
     global total_signals                                                              
-    total_signals = zeros(1000)                                                       
+    total_signals = zeros(1000)                                                     
     list_of_objects.append(Signal(frequency=frequency, amplitude=amplitude))          
     for object in list_of_objects:                                                    
         object_frequency = object.frequency                                           
@@ -102,103 +103,68 @@ def removing_signal(removed_freq,removed_amp):
         object_amplitude = object.amplitude
         signal_y_axis = object_amplitude*sin(2*pi*object_frequency*initial_time)
         total_signals += signal_y_axis
-
 # ------------------------------------------------------------------------------------Sampling Added Signals
 def Sampling_added_signals(total_signals):
 
-    
+
+    N,I = st.sidebar.columns(2)
+    contain = st.container()
+
+    Original_Graph = st.checkbox('Original Graph',value=True,key='Original_Graph 123')
+    with I:
+        interpolation_check_box = st.checkbox('Interpolation',key='interpolation_check_box 132')
+    with N:
+        noise = st.checkbox('Noise' ,key="Noise Check Box 3432")
+    with N:
+        sampling = st.checkbox('Sampling Points' ,key="sampling Check Box 4232")
+
     sampling_method = st.sidebar.selectbox("The way of sampling",["Using F.maximum","Using sampling frequency"])
     if sampling_method == "Using F.maximum":
         max_freq = st.sidebar.slider(label= "F.maximum (Hz)",min_value=1,max_value=100,value=1,step=1)
         sampling_frequency = 2 * max_freq
     elif sampling_method == "Using sampling frequency":
         sampling_frequency = st.sidebar.slider(label='Sampling Frequency (Hz)', min_value=1, max_value=100, value=1, step=1)
-    
-    sampling_period=1/sampling_frequency
 
     sample_rate = int((1000/2)/(sampling_frequency))
-    if sample_rate == 0:
-        sample_rate = 1
+    time_axis = linspace(0, 2, 1000)
+    sampled_time_axis= time_axis[::sample_rate]
     
-    time_axis      = linspace(0, 2, 1000)                 
-    sampled_time_axis      = time_axis[::sample_rate]  
+    time_matrix = resize(time_axis, (len(sampled_time_axis), len(time_axis)))
+    K = (time_matrix.T - sampled_time_axis) / (sampled_time_axis[1] - sampled_time_axis[0])
+
+    if noise:
+        #------------------- adding and sampling noise 
+        noise_signal=add_noise()
+        sampled_signals = noise_signal[::sample_rate]
+        total_signals=noise_signal
+        #------------------Reconstructed  signals of noise
+        final_matrix = sampled_signals * sinc(K)
+    else:
+        #----------------- sampling signals without noise
+        sampled_signals= total_signals[::sample_rate]
+        #------------------------------ Reconstruct noise signal with noise -------------------------------#
+        final_matrix = sampled_signals * sinc(K)
+
+    reconstructed_signal = sum(final_matrix, axis=1)
 
     fig, axs = plt.subplots()
     fig.set_size_inches(11, 3.5)
 
-    noise_signal=add_noise()
-    noise_sampled_y_axis = noise_signal[::sample_rate]
-    N,I = st.sidebar.columns(2)
-    contain = st.container()
-    Original_Graph = st.checkbox('Original Graph',value=True,key='Original_Graph 123')
-    with I:
-        interpolation_check_box = st.checkbox('Interpolation',key='interpolation_check_box 132')
-    with N:
-        noise = st.checkbox('Noise',value=True ,key="Noise Check Box 3432")
+    if interpolation_check_box:
+        axs.plot(time_axis,reconstructed_signal,color='Red',linestyle='dashed',alpha=1)
 
-    if noise and interpolation_check_box and Original_Graph :
-
-        time_matrix = resize(time_axis, (len(sampled_time_axis), len(time_axis)))
-        K = (time_matrix.T - sampled_time_axis) / (sampled_time_axis[1] - sampled_time_axis[0])
-        final_matrix = noise_sampled_y_axis * sinc(K)
-        reconstructed_signal = sum(final_matrix, axis=1)
-
-        axs.plot(sampled_time_axis, noise_sampled_y_axis ,color='yellow' ,marker="o" ,linestyle='',alpha=0.7)
-        axs.plot(time_axis,reconstructed_signal,color='Red',linestyle='dashed',alpha=0.7)
-        axs.plot(time_axis,noise_signal, color='royalblue', alpha=0.4)
-    
-    elif noise and interpolation_check_box :
-
-        time_matrix = resize(time_axis, (len(sampled_time_axis), len(time_axis)))
-        K = (time_matrix.T - sampled_time_axis) / (sampled_time_axis[1] - sampled_time_axis[0])
-        final_matrix = noise_sampled_y_axis * sinc(K)
-        reconstructed_signal = sum(final_matrix, axis=1)
-
-        axs.plot(sampled_time_axis, noise_sampled_y_axis ,color='yellow' ,marker="o" ,linestyle='',alpha=0.7)
-        axs.plot(time_axis,reconstructed_signal,color='Red',linestyle='dashed',alpha=0.7)
-    
-    elif noise and Original_Graph :
-
-        total_signals_sampled= total_signals[::sample_rate]
-        axs.plot(sampled_time_axis, total_signals_sampled ,color='yellow' ,marker="o" ,linestyle='',alpha=0.7)
-        axs.plot(time_axis,noise_signal, color='royalblue' ,linewidth=1,  alpha=0.4)
-
-    elif interpolation_check_box and Original_Graph :
-
-        total_signals_sampled= total_signals[::sample_rate]
-        time_matrix = resize(time_axis, (len(sampled_time_axis), len(time_axis)))
-        K = (time_matrix.T - sampled_time_axis) / (sampled_time_axis[1] - sampled_time_axis[0])
-        final_matrix = total_signals_sampled * sinc(K)
-        reconstructed_signal = sum(final_matrix, axis=1)
-
-        axs.plot(time_axis,reconstructed_signal,color='Red',linestyle='dashed',alpha=0.9)
-        axs.plot(sampled_time_axis, total_signals_sampled, color='yellow', marker="o" ,linestyle="")
-        axs.plot(time_axis,total_signals, color='royalblue',alpha=0.4)
-
-    elif noise:
-        axs.plot(sampled_time_axis, noise_sampled_y_axis ,color='yellow' ,marker="o" ,linestyle='')
-
-    elif interpolation_check_box:
-
-        total_signals_sampled= total_signals[::sample_rate]
-        time_matrix = resize(time_axis, (len(sampled_time_axis), len(time_axis)))
-        K = (time_matrix.T - sampled_time_axis) / (sampled_time_axis[1] - sampled_time_axis[0])
-        final_matrix = total_signals_sampled * sinc(K)
-        reconstructed_signal = sum(final_matrix, axis=1)
-
-        axs.plot(time_axis,reconstructed_signal,color='Red',linestyle='dashed',alpha=0.7)
-        axs.plot(sampled_time_axis, total_signals_sampled ,color='yellow' ,marker="o" ,linestyle='',alpha=0.7)
-
-    elif Original_Graph:
-
-        total_signals_sampled= total_signals[::sample_rate]
-        axs.plot(sampled_time_axis, total_signals_sampled ,color='yellow' ,marker="o" ,linestyle='',alpha=0.7)
+    if Original_Graph:
         axs.plot(time_axis,total_signals, color='royalblue')
 
-    else:
-        total_signals_sampled= total_signals[::sample_rate]
-        axs.plot(sampled_time_axis, total_signals_sampled, color='yellow' , marker="o" ,linestyle="")
+    if sampling:
+        axs.plot(sampled_time_axis, sampled_signals, color='green' , marker="o" ,linestyle="" ,alpha=0.7)
+    
+    
+    x = linspace(0,2,1000)
+    y = zeros(1000)
+    axs.plot(x , y, color='grey', alpha = 0.5)
 
+    plt.xlim(0,2)
     font1 = {'family':'serif','color':'white','size':20}
     plt.xlabel("Time (seconds)",fontdict = font1)
     plt.ylabel("Amplitude",fontdict = font1)
@@ -207,16 +173,13 @@ def Sampling_added_signals(total_signals):
 # ------------------------------------------------------------------------------------Adding Noise
 def add_noise():
 
-    SNR = st.sidebar.slider(label='SNR', min_value=0.0, max_value=50.0, value=0.0, step=0.1)
+    SNR = st.sidebar.slider(label='SNR', min_value=1, max_value=50, value=25, step=1)
 
     signal_power = total_signals **2                                    # Generating the signal power
     
     signal_power_avg = mean(signal_power)                               # mean of signal power
 
-    if (SNR==0):
-        noise_power = signal_power_avg / 0.0000001
-    else:
-        noise_power = signal_power_avg / SNR
+    noise_power = signal_power_avg / SNR
     mean_noise = 0
     noise = random.normal(mean_noise,sqrt(noise_power),len(total_signals))
     noise_signal = total_signals + noise
--- Sampling_Studio/main.py
+++ Sampling_Studio/main.py
@@ -6,50 +6,30 @@ import generated_signal_fn as GSF
 import numpy as np
 
 # ------------------------------------------------------------------------------------Front end 
-
 st.set_page_config(layout="wide")
 container = st.sidebar.container()
-#  autorangeChart() {
-#         Plotly.relayout('plotly', {
-#             'xaxis.autorange': true,
-#             'yaxis.autorange': true
-#         });
-# }
-# st.markdown("<script></script>",unsafe_allow_html=True)
+
 with open("design.css") as source_ds:
     st.markdown(f"<style>{source_ds.read()}</style>",unsafe_allow_html=True)
 
 # ------------------------------------------------------------------------------------ User Options
-options = st.sidebar.radio('Site Tabs', options=['Uploaded Signal Studio','Signal Generation Studio'])
-
-
-    
+dataset = container.file_uploader("", type = ['csv'])
 
 # ------------------------------------------------------------------------------------Uploaded Signal Studio
-def uploaded_signal_studio():
-    if dataset is not None:
-        df = pd.read_csv(dataset)
-        df_x_axis,df_y_axis = USF.get_data_frame(df)
-        USF.add_signal(df)
-        sampled_amplitude, sampled_time ,df_y_axis = USF.signal_sampling(df)
-        time_points, reconstructed_signal = USF.signal_reconstructing(df, sampled_time, sampled_amplitude)
-        USF.sampling_signal_plotting(df,df_y_axis,sampled_time,sampled_amplitude)
-        USF.general_signal_plotting(time_points, reconstructed_signal)
-        # st.sidebar.download_button('Download Your Data', df.to_csv(),file_name= f'Data With Code #{randint(0, 1000)}.csv' ,mime = 'text/csv',key="Download Button 42332")
-    else:
-        # st.header("Upload Your Signal to Start Applying Functions")
-        GSF.sin_signal_viewer()
-# ------------------------------------------------------------------------------------Generated Signal Studio
 def generated_signal_studio():
     total_signals=GSF.add_signal()
     GSF.Sampling_added_signals(total_signals)
+
+if dataset is not None:
+    df = pd.read_csv(dataset)
+    total_signals = USF.add_signal(df)
+    USF.signal_sampling(df,total_signals)
+    # st.sidebar.download_button('Download Your Data', df.to_csv(),file_name= f'Data With Code #{randint(0, 1000)}.csv' ,mime = 'text/csv',key="Download Button 42332")
+else:
+    generated_signal_studio()
+
+# ------------------------------------------------------------------------------------Generated Signal Studio
+
     # time = np.linspace(0,2,1000)
     # df = pd.DataFrame({'Time (sc)': time, 'Amplitudes (m)': total_signals}, columns=['Time (sc)','Amplitudes (m)'])
     # st.sidebar.download_button('Download Your Data', df.to_csv(index=False),file_name= f'Data With Code #{randint(0, 1000)}.csv' ,mime = 'text/csv',key="Download Button 11276")
-
-# ------------------------------------------------------------------------------------Radio Buttons
-if options == 'Uploaded Signal Studio':
-    dataset = container.file_uploader("", type = ['csv'])
-    uploaded_signal_studio()
-elif options == 'Signal Generation Studio':
-    generated_signal_studio()
