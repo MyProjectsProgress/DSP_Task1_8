f3909fbaae31b45b6e86b45959f807afa77a4fc5
MagdyNasr41 <90320433+MagdyNasr41@users.noreply.github.com>
2022-10-25 02:04:02 +0200

    main updated

--- Sampling_Studio/main.py
+++ Sampling_Studio/main.py
@@ -6,17 +6,10 @@ import uploaded_signals_fn as USF
 import generated_signal_fn as GSF
 import numpy as np
 
-# ------------------------------------------------------------------------------------Front end 
-
-# container = st.sidebar.container()
-
-# with open("design.css") as source_ds:
-#     st.markdown(f"<style>{source_ds.read()}</style>",unsafe_allow_html=True)
-
 # ------------------------------------------------------------------------------------ User Options
 dataset = st.file_uploader(label="Uploading Signal", type = ['csv'])
 
-# ------------------------------------------------------------------------------------Uploaded Signal Studio
+# ------------------------------------------------------------------------------------Uploaded and Genertaed Signal Studios
 def generated_signal_studio():
     total_signals=GSF.add_signal()
     reconstructed_signal = GSF.Sampling_added_signals(total_signals)
@@ -31,6 +24,5 @@ if dataset is not None:
     time = np.linspace(begin_time,end_time,len(reconstructed_signal))
     df = pd.DataFrame({'Time (sc)': time, 'Amplitudes (m)': reconstructed_signal}, columns=['Time (sc)','Amplitudes (m)'])
     st.sidebar.download_button('Download Your Data', df.to_csv(index=False),file_name= f'Data With Code #{randint(0, 1000)}.csv' ,mime = 'text/csv',key="Download Button 2022")
-    # st.sidebar.download_button('Download Your Data', df.to_csv(),file_name= f'Data With Code #{randint(0, 1000)}.csv' ,mime = 'text/csv',key="Download Button 42332")
 else:
     generated_signal_studio()
